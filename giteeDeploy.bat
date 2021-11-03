d41f11fa02f054556754ec24f544a0aacf4dd630
a1667834841 <167834841@qq.com>
2021-11-03 22:20:41 +0800

    本地

--- giteeDeploy.bat
+++ giteeDeploy.bat
@@ -11,13 +11,13 @@ cd  d:
 cd D:\project\vscode\theme-vdoing-blog\docs\.vuepress\dist
 
 
-git config --global user.name "ggball"
+git config --global user.name "a1667834841"
 git config --global user.email "167834841@qq.com"
 
 git init
 git add -A
-git commit -m "来自gitee的部署"
-git remote add origin https://gitee.com/zxqzhuzhu/theme-vdoing-blog.git
-git push -f https://gitee.com/zxqzhuzhu/theme-vdoing-blog.git master:page # 推送到github page分支
+git commit -m "本地"
+git remote add origin https://github.com/a1667834841/blog.git
+git push -f https://github.com/a1667834841/blog.git master:page # 推送到github page分支
 
 echo '成功提交'
