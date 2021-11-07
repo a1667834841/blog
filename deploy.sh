96a5e14c4037e2d72e80ca799f17df41f7b3c80c
a1667834841 <167834841@qq.com>
2021-11-07 20:51:26 +0800

    15

--- deploy.sh
+++ deploy.sh
@@ -26,12 +26,12 @@ git init
 git add -A
 git commit -m "${msg}"
 git remote add origin https://a1667834841:${GITHUB_TOKEN}@github.com/a1667834841/blog.git
-# git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
-git push -f master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+
 
 # if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
 #   codingUrl=git@e.coding.net:xgy/xgy.git
@@ -41,6 +41,7 @@ git push -f master:gh-pages # 推送到github gh-pages分支
 # git add -A
 # git commit -m "${msg}"
 # git push -f $codingUrl master # 推送到coding
+
 
 cd -
 rm -rf docs/.vuepress/dist
