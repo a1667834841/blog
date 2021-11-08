abf08c938debd9c35c6da1b2e58716b3de3e8a1f
a1667834841 <167834841@qq.com>
2021-11-08 18:41:56 +0800

    提交coding

--- deploy.sh
+++ deploy.sh
@@ -11,35 +11,35 @@ cd docs/.vuepress/dist
 # deploy to github pages
 # echo 'ggball.top' > CNAME
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com/a1667834841/blog.git
-  git config --global user.name "a1667834841"
-  git config --global user.email "1667834841@qq.com"
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://a1667834841:${GITHUB_TOKEN}@github.com/a1667834841/blog.git
-  git config --global user.name "a1667834841"
-  git config --global user.email "1667834841@qq.com"
-fi
-git init
-git add -A
-git commit -m "${msg}"
-git remote add origin https://a1667834841:${GITHUB_TOKEN}@github.com/a1667834841/blog.git
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+# if [ -z "$GITHUB_TOKEN" ]; then
+#   msg='deploy'
+#   githubUrl=git@github.com/a1667834841/blog.git
+#   git config --global user.name "a1667834841"
+#   git config --global user.email "1667834841@qq.com"
+# else
+#   msg='来自github actions的自动部署'
+#   githubUrl=https://a1667834841:${GITHUB_TOKEN}@github.com/a1667834841/blog.git
+#   git config --global user.name "a1667834841"
+#   git config --global user.email "1667834841@qq.com"
+# fi
+# git init
+# git add -A
+# git commit -m "${msg}"
+# git remote add origin https://a1667834841:${GITHUB_TOKEN}@github.com/a1667834841/blog.git
+# git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:personal/blog.git
-# else
-#   codingUrl=https://ggball:${CODING_TOKEN}@e.coding.net/personal/blog.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
+if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  codingUrl=git@e.coding.net:personal/blog.git
+else
+  codingUrl=https://ggball:${CODING_TOKEN}@e.coding.net/personal/blog.git
+fi
+git add -A
+git commit -m "${msg}"
+git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
