62d49126aa237f1a2e11a7364787fefef222a94c
a1667834841 <1667834841@qq.com>
2021-11-22 13:00:25 +0800

    推送地址修改

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,7 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.ggball.top&token=IXN3sgv5gnMqQE0N"
+# curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.ggball.top&token=IXN3sgv5gnMqQE0N"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://ggball.top&token=IXN3sgv5gnMqQE0N"
 
 rm -rf urls.txt # 删除文件
