fd17805b88da1abec666e35b9fefc5a0307ac84b
ggball <1667834841@qq.com>
2022-10-23 21:32:04 +0800

    plugin 修改

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -9,35 +9,35 @@ module.exports = [
   'vuepress-plugin-baidu-autopush', // 百度自动推送
 
   // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
-  [
-    'thirdparty-search',
-    {
-      thirdparty: [
-        // 可选，默认 []
-        {
-          title: '在MDN中搜索',
-          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-          behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-        },
-        {
-          title: '在Runoob中搜索',
-          frontUrl: 'https://www.runoob.com/?s=',
-        },
-        {
-          title: '在Vue API中搜索',
-          frontUrl: 'https://cn.vuejs.org/v2/api/#',
-        },
-        {
-          title: '在Bing中搜索',
-          frontUrl: 'https://cn.bing.com/search?q=',
-        },
-        {
-          title: '通过百度搜索本站的',
-          frontUrl: 'https://www.baidu.com/s?wd=site%3Aggball.top%20',
-        },
-      ],
-    },
-  ],
+  // [
+  //   'thirdparty-search',
+  //   {
+  //     thirdparty: [
+  //       // 可选，默认 []
+  //       {
+  //         title: '通过谷歌搜索本站的',
+  //         frontUrl: 'https://www.google.com/search?q=',
+  //       },
+  //       {
+  //         title: '在MDN中搜索',
+  //         frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+  //         behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+  //       },
+  //       {
+  //         title: '在Bing中搜索',
+  //         frontUrl: 'https://cn.bing.com/search?q=',
+  //       },
+  //       {
+  //         title: '通过百度搜索本站的',
+  //         frontUrl: 'https://www.baidu.com/s?wd=site%3Aggball.top%20',
+  //       },
+  //     ],
+  //   },
+  // ],
+
+  ['@vuepress/search', {
+    searchMaxSuggestions: 10
+  }],
 
   [
     'one-click-copy',
